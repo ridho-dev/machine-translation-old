d58111a0cbc90cac134789be4ecf55ef08647f63
ridho-dev <ridhopardede.dev@gmail.com>
2023-04-19 11:58:43 +0700

    add text input

--- app/src/main/java/com/dededev/machinetranslation/MainActivity.kt
+++ app/src/main/java/com/dededev/machinetranslation/MainActivity.kt
@@ -2,10 +2,43 @@ package com.dededev.machinetranslation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import com.dededev.machinetranslation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val text = binding.inputSource
+        val translation = binding.targetSource
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                "Not yet implemented"
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                "Not yet implemented"
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    if (p0.isNotEmpty()) {
+                        translation.visibility = View.VISIBLE
+                    } else {
+                        translation.visibility = View.GONE
+                    }
+                }
+            }
+        }
+
+        text.addTextChangedListener(textWatcher)
+
     }
 }
